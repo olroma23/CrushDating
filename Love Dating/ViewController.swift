7af7778c1c57e2e3ba5b65322eed30dcd70e31b6
olroma23 <roliynyk68@gmail.com>
2020-07-23 20:20:59 +0300

    Initial Commit

---
+++ Love Dating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Love Dating
+//
+//  Created by Roman Oliinyk on 23.07.2020.
+//  Copyright © 2020 Roman Oliinyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
